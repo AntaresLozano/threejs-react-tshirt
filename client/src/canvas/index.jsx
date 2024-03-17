8d689cfe763413ec87c753f634beceb375d8acac
Antares lozano <dizziness1948@gmail.com>
2024-03-17 18:56:34 -0500

    layout and 3d model

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,7 +1,22 @@
-const Canvas = () => {
-  return (
-    <div>canvas</div>
-  )
-}
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
+// import { BackDrop } from "./BackDrop";
+// import { CameraRig } from "./CameraRig";
+import Shirt  from "./Shirt";
 
-export default Canvas
+const CanvasModel = () => {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+      {/* <CameraRig>
+        <BackDrop /> */}
+        <Center>
+          <Shirt />
+        </Center>
+      {/* </CameraRig> */}
+    </Canvas>
+  );
+};
+
+export default CanvasModel;
